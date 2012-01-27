5ed5e20f1c9f6f405625b0a989f59c2d5671e3b5
Stephen Johnson <steve@thatbytes.co.uk>
2012-01-27 12:01:02 +0000

    spacing yet again

--- lib/producteev/api.rb
+++ lib/producteev/api.rb
@@ -3,55 +3,54 @@ module Producteev
       include HTTParty
       base_uri 'https://api.producteev.com/'
 
-    def initialize(apikey,secret, debug)
-      @apikey = apikey
-      @secret = secret
-      @token = nil
-      @debug = debug
-    end
+      def initialize(apikey,secret, debug)
+        @apikey = apikey
+        @secret = secret
+        @token = nil
+        @debug = debug
+      end
 
-    def login(email,password)
-      param = {:email => email, :password => password}
-      @token = self.sendRequest("/users/login.json",param)['login']['token']
-    end
+      def login(email,password)
+        param = {:email => email, :password => password}
+        @token = self.sendRequest("/users/login.json",param)['login']['token']
+      end
 
-    def generateSig(parameters)
-      signature = ""
-     
-      #sort the hash alphabetically
-      parameters = parameters.sort
+      def generateSig(parameters)
+        signature = ""
+       
+        #sort the hash alphabetically
+        parameters = parameters.sort
 
-      parameters.each { |key,value|  
-        if (value.kind_of? String or value.kind_of? Integer)
-          signature = "#{signature}#{key}#{value}"
+        parameters.each { |key,value|  
+          if (value.kind_of? String or value.kind_of? Integer)
+            signature = "#{signature}#{key}#{value}"
+          end
+        }
+        signature = signature+@secret
+        signature = Digest::MD5.hexdigest(signature)
+        return signature
+      end
+
+
+      def sendRequest(path, options={})
+        options.merge!({:api_key => @apikey})
+
+        if @token != nil
+          options.merge!({:token => @token})
         end
-      }
-      signature = signature+@secret
-      signature = Digest::MD5.hexdigest(signature)
-      return signature
-    end
+        options.merge!({:api_sig => generateSig(options)})
 
+        if @debug
+          response = self.class.get(path, {:query => options, :debug_output => $stderr})
+        else
+          response = self.class.get(path, {:query => options})
+        end
 
-    def sendRequest(path, options={})
-      options.merge!({:api_key => @apikey})
-
-      if @token != nil
-        options.merge!({:token => @token})
+        if response.success?
+          return JSON.parse(response.body)
+        else
+          raise response.response    
+        end    
       end
-      options.merge!({:api_sig => generateSig(options)})
-
-      if @debug
-        response = self.class.get(path, {:query => options, :debug_output => $stderr})
-      else
-        response = self.class.get(path, {:query => options})
-      end
-
-      if response.success?
-        return JSON.parse(response.body)
-      else
-        raise response.response    
-      end
-      
-    end
   end
 end
