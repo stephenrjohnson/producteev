4ed1806c1d4fe8dcce73cd7fc318e04cacfffa31
Stephen Johnson <steve@thatbytes.co.uk>
2012-02-03 16:20:50 +0000

    push version ready for update

--- lib/producteev/version.rb
+++ lib/producteev/version.rb
@@ -1,8 +1,8 @@
 module Producteev
   module VERSION
     MAJOR = 1
-    MINOR = 0
-    PATCH = 2
+    MINOR = 1 
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
